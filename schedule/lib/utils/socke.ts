9e30d5120343951a1ba08a41af784c0b2c354975
Lester-Alonzo <60448940+Lester-Alonzo@users.noreply.github.com>
2023-07-18 10:44:06 -0600

    18-07-2023 | ENV

--- schedule/lib/utils/socke.ts
+++ schedule/lib/utils/socke.ts
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
-const url = process.env.SOCKET_URL || "http://localhost:3001"
+const url = process.env.SOCKET_URL || "http://10.0.1.200:3001"
 
 export const socket = io(url,  {
     transports: ['websocket'],
