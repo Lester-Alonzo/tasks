b10c6dd56ce20b4f15915d5eba3e3b419f4884c2
Lester-Alonzo <60448940+Lester-Alonzo@users.noreply.github.com>
2023-09-11 17:02:33 -0600

    [:construction: 08/09]: done

--- schedule/lib/global/picker/Date.tsx
+++ schedule/lib/global/picker/Date.tsx
@@ -4,11 +4,12 @@ import {useRef} from 'react'
 export function Datess ({id, fn}:{id:number, fn:(date:string) => void}) {
     const iref = useRef<HTMLInputElement>(null)
     function DateParse(date:string) {
-        const d = new Date(date)
+        let fi = date.split('-')
+        const d = new Date(parseInt(fi[0]), parseInt(fi[1]) - 1, parseInt(fi[2]))
         const day = d.getDate()
         const month = d.getMonth()
         const year = d.getFullYear()
-        return `${day + 1}/${month}/${year}`
+        return `${day}/${month + 1}/${year}`
     }
     const handleClck = () => {
         if(iref.current === null) return
@@ -17,7 +18,7 @@ export function Datess ({id, fn}:{id:number, fn:(date:string) => void}) {
     }
 return (
     <div className={styles.gnmain}>
-        <input type="date" ref={iref} style={{borderRadius:"11px", padding:"1rem", border:"none"}}/>
+        <input type="date" ref={iref} style={{borderRadius:"11px", padding:"1rem", border:"none"}} onChange={() => console.log(iref.current?.value)}/>
         <button onClick={handleClck}>Listo</button>
     </div>
 )
