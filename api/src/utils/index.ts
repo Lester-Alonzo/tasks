0163a2a8ed30407d542c66f03418d9d12c3e0941
Lester-Alonzo <60448940+Lester-Alonzo@users.noreply.github.com>
2023-09-12 09:50:55 -0600

    [:construction: 08/09]: done

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -3,5 +3,5 @@ export function ParseDate():string {
     const day = date.getDate()
     const month = date.getMonth()
     const year = date.getFullYear()
-    return `${day}/${month}/${year}` 
+    return `${day}/${month + 1}/${year}` 
 }
